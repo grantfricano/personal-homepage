afe6dbef1325b820dc3dcb573c945d4b0455cbac
Grant Alexander <grant.fricano@gmail.com>
2022-09-17 14:33:09 -0400

    working on background image transitions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import '../App.css';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { CSSTransition } from 'react-transition-group'
 
 
 const motorcyclePicture = new URL('../images/SF-19.jpg', import.meta.url);
@@ -20,21 +21,22 @@ render() {
 
   return ( 
     <div>
-    <img class='image' id='backgroundPic' src={this.state.backgroundImage}></img>
-      <div class="firstname top-left">Grant<br/><span class="lastname">Fricano</span></div>
-      
-      <div class='bottom-left'>
-        <div>
-          <button class='btn btn-primary button-details' onClick={() => this.changeBackgroundImage(motorcyclePicture1)}>About</button>
-          <button class='btn btn-primary button-details' onClick={() => this.changeBackgroundImage(motorcyclePicture)}>Projects</button>
+        <CSSTransition classNames="fade" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+            <img class='image' id='backgroundPic' src={this.state.backgroundImage}></img>
+        </CSSTransition>
+     
+        <div class="firstname top-left">Grant<br/><span class="lastname">Fricano</span></div>
+        <div class='bottom-left'>
+            <div>
+                <button class='btn btn-primary button-details' onClick={() => this.changeBackgroundImage(motorcyclePicture1)}>About</button>
+                <button class='btn btn-primary button-details' onClick={() => this.changeBackgroundImage(motorcyclePicture)}>Projects</button>
+            </div>
+            <div>
+                <button class='btn btn-primary button-details'>Contact</button>
+                <button class='btn btn-primary button-details'>Edit</button>
+            </div>
         </div>
-        <div>
-          <button class='btn btn-primary button-details'>Contact</button>
-          <button class='btn btn-primary button-details'>Edit</button>
-        </div>
-      </div>
     </div>
-    
   );
 }
 }
