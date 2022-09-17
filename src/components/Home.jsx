bec5ad61265f8dd6e6dc1972c6fe98102d5bbcc2
Grant Alexander <grant.fricano@gmail.com>
2022-09-17 14:06:30 -0400

    setting up project for personal website

---
+++ src/components/Home.jsx
@@ -0,0 +1,42 @@
+import '../App.css';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+
+const motorcyclePicture = new URL('../images/SF-19.jpg', import.meta.url);
+const motorcyclePicture1 = new URL('../images/SF-10.jpg', import.meta.url);
+
+class Home extends Component {
+
+    state = {
+        backgroundImage: motorcyclePicture
+    };
+
+    changeBackgroundImage = (image) => {
+        this.setState({ backgroundImage: image });
+    }
+
+render() {
+
+  return ( 
+    <div>
+    <img class='image' id='backgroundPic' src={this.state.backgroundImage}></img>
+      <div class="firstname top-left">Grant<br/><span class="lastname">Fricano</span></div>
+      
+      <div class='bottom-left'>
+        <div>
+          <button class='btn btn-primary button-details' onClick={() => this.changeBackgroundImage(motorcyclePicture1)}>About</button>
+          <button class='btn btn-primary button-details' onClick={() => this.changeBackgroundImage(motorcyclePicture)}>Projects</button>
+        </div>
+        <div>
+          <button class='btn btn-primary button-details'>Contact</button>
+          <button class='btn btn-primary button-details'>Edit</button>
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+}
+
+export default Home;
